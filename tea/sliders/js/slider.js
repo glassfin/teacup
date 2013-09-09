0977d72a3684b31f04ae12f9769156b13d757379
Knight W. Fu <knight.w.fu@gmail.com>
2013-09-09 13:04:33 -0400

    added backbone model and view

--- tea/sliders/js/slider.js
+++ tea/sliders/js/slider.js
@@ -33,30 +33,69 @@
 	 label : fieldset.find('label.title').html()
       };
 
-      console.log( model );
+      return model;
    }
 
-   $(document).ready(function( event )
-   {
-      $('fieldset[type="slider"]').each( function( index, object )
-      {
-	 getModelFromHTML($(object));
-      });
-   });
+$(document).ready(function( event )
+{
 
 ////
 
-Backbone.Model.extend({
+var SliderModel = Backbone.Model.extend({
    defaults: {
    },
 
    initialize : function( modelObj )
    {
+      this.min = modelObj.min;
+
+      this.max = modelObj.max;
+
       // if the value is not defined, then set it to
       // the min
-      this.value = modelObj.value || modelObj.min;
+      this.value = modelObj.value? modelObj.value : modelObj.min;
       
+      this.label = modelObj.label;
    }
+});
+
+var SliderView = Backbone.View.extend( {
+
+   // compiling a template to be used for
+   // base rendering
+   baseTpl : Handlebars.compile($('#tea-slider-template').html()),
+
+   render : function()
+   {
+      var modelJSON = this.model.toJSON();
+
+      modelJSON.value = modelJSON.value? 
+	 modelJSON.value : modelJSON.min;
+
+      var html = this.baseTpl( this.model.toJSON() );
+
+      console.log( html );
+     
+      this.$el.html( html );
+   }
+
+});
+
+
+$('fieldset[type="slider"]').each( function( index, object )
+{
+   var modelObj = getModelFromHTML($(object));
+
+   var sliderModel = new SliderModel( modelObj );
+
+   var sliderView = new SliderView( { model : sliderModel } );
+
+   sliderView.render();
+   
+   $('body').append( sliderView.el );
+
+});
+
 });
 
 ////
